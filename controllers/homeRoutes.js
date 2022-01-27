c5413d405d0ae8630f0a3ab9e4a35e0b8b2515cc
ashleysalinas <ashleysalinas98@protonmail.com>
2022-01-27 11:23:33 -0600

    added profile page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -48,9 +48,39 @@ router.get('/post/:id', async (req,res) => {
         res.status.json(err)
     }
 })
-router.get('/post', withAuth, (req, res) => {
+router.get('/post', withAuth, async (req, res) => {
     try {
         res.render('newPost', {
+            logged_in:true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                id: req.session.user_id
+            }}
+        )
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            }
+        })
+        const commentData = await Comment.findAll({
+            where: {
+                user_id: req.session.user_id
+            }
+        })
+        const posts = postData.map((post) => post.get({ plain: true }))
+        const comments = commentData.map((comment) => comment.get({plain: true}))
+        const user = userData.get({ plain: true})
+        res.render('profile', {
+            ...user,
+            posts,
+            comments,
             logged_in:true
         });
     } catch (err) {
