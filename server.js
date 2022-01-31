6dacb80d5f6131fae489fc6771682b2c9a9144e3
ashleysalinas <ashleysalinas98@protonmail.com>
2022-01-30 20:17:25 -0600

    gitignore

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes)
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(3001, () => {
+    app.listen(process.env.PORT, () => {
         console.log('Now listening on 3001')
     })
 })
